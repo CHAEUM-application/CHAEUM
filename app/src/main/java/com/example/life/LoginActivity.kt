d640e81986e9a86455955fd39f2bf66105904146
chilyoon <nby5699@gmail.com>
2023-05-21 01:19:18 +0900

    로그인 화면 구성

--- app/src/main/java/com/example/life/LoginActivity.kt
+++ app/src/main/java/com/example/life/LoginActivity.kt
@@ -3,32 +3,41 @@ package com.example.life
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
-import android.widget.DatePicker
 import android.widget.EditText
+import android.widget.Toast
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var editTextID: EditText
+    private lateinit var editTextPW: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.loginpage)
 
-        val nameText = findViewById<EditText>(R.id.nameText)
-        val joinButton = findViewById<Button>(R.id.joinButton)
-        val datePicker = findViewById<DatePicker>(R.id.datepicker)
+        editTextID = findViewById(R.id.editTextID)
+        editTextPW = findViewById(R.id.editTextPW)
 
-        joinButton.setOnClickListener {
-            val year = datePicker.year
-            val month = datePicker.month
-            val dayOfMonth = datePicker.dayOfMonth
+        val loginButton: Button = findViewById(R.id.loginButton)
+        val backButton: Button = findViewById(R.id.backButton)
 
-            val intent = Intent(this, MainActivity::class.java).apply {
-                putExtra("name", nameText.text.toString())
-                putExtra("year", year)
-                putExtra("month", month)
-                putExtra("dayOfMonth", dayOfMonth)
+        loginButton.setOnClickListener {
+            val id = editTextID.text.toString().trim()
+            val pw = editTextPW.text.toString().trim()
+
+            if (id.isEmpty() || pw.isEmpty()) {
+                Toast.makeText(this, "ID와 PW를 입력해주세요.", Toast.LENGTH_SHORT).show()
+            } else {
+                // 로그인 성공 시 MainActivity로 이동
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+                finish() // 현재 액티비티를 종료하여 뒤로가기 시 돌아올 수 없도록 함
             }
-            startActivity(intent)
+        }
+
+        backButton.setOnClickListener {
+            onBackPressed()
         }
     }
 }
